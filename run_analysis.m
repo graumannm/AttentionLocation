caf5fb8d573e4bb08c73f1d6a0c6de34438e3a98
monikag <graumannm@login.curta.zedat.fu-berlin.de>
2023-03-22 16:42:06 +0100

    added EEG plotting

--- run_analysis.m
+++ run_analysis.m
@@ -1,6 +1,9 @@
 % Run Analysis
 
 %% fMRI decoding
+% whole sample
+% should take a few seconds
+
 subjects = [1:3 5:9 11:16 18:23];
 
 % takes a few seconds
@@ -14,6 +17,26 @@ for iroi = 1:length(ROIs)
     plot_fMRI_result(iroi);
 end
 
-%% EEG decoding
+%% EEG decoding 
+% for single example subject
+% Duration: ~1h 15 min for 10 permutations
+
+steps        = 20; % downsample time in 20 ms steps for speed. 
+% Original parameters in manuscript: 
+%       1 ms for time-resolved analysis
+%       10 ms for time-generalization analysis
+permutations = 5; % for speed. Original: 100 permutations
+sbj          = 5;
+
+% run classification analysis for this subject
+EEG_decoding(steps,permutations,sbj);
+
+% plot time-generalization results
+plot_TGM(sbj);
+
+% plot time-resolved results
+plot_timecourse(sbj);
+
+
 
 
